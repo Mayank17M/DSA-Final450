7e924431ec5d261c5dc0a7ef767426fa4a86b990
Mayank17M <mamgain.mayank01@gmail.com>
2021-09-15 19:36:54 +0530

    string: longest palindromic substring

---
+++ String/longest_palindromic_substring.cpp
@@ -0,0 +1,8 @@
+/** 
+ * Program to find Longest Palindrome in a String.
+ * 
+ * References: https://www.geeksforgeeks.org/longest-palindrome-substring-set-1/
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
