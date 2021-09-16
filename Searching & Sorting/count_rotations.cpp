d7a6cdbf512d13e8bdb5b1bc826ef6a8198ee741
Mayank17M <mamgain.mayank01@gmail.com>
2021-09-16 21:05:37 +0530

    Count rotations

---
+++ Searching & Sorting/count_rotations.cpp
@@ -0,0 +1,40 @@
+/** 
+ * Consider an array of distinct numbers sorted in increasing order. 
+ * The array has been rotated (clockwise) k number of times. 
+ * Given such an array, find the value of k. 
+ * 
+ * References: 
+ *  https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
+ *  https://www.youtube.com/watch?v=4WmTRFZilj8
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int countRotations(int arr[], int low, int high){
+    int n = high+1;
+    if(high < low) return 0;
+    if (high == low) return low;
+    while(low<=high){
+        int mid = (high + low) / 2;
+        int prev = (mid+n-1)%n;
+        int next = (mid +1)%n;
+        if(arr[mid]<=arr[next] && arr[mid]<=arr[prev]){
+            return mid;
+        }
+        if(arr[low]<=arr[mid]){
+            low = mid+1;
+        }
+        else if(arr[mid]<=arr[high]){
+            high = mid-1;
+        }
+    }
+}
+
+int main(){
+    int arr[] = {15, 18, 2, 3, 6, 12};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << countRotations(arr, 0, n-1);
+    return 0;
+}
+
